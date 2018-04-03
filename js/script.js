91f5298835adca6843295a11eca5fc5236aa5904
Irina Capatina <irina.capatina@yahoo.com>
2018-04-04 00:07:06 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -35,3 +35,28 @@ slideButtonThree.addEventListener("click", function (evt) {
     slideButtonTwo.classList.remove("slider-btn-active");
     slideButtonThree.classList.add("slider-btn-active");
 });
+
+
+  var mapLink = document.querySelector(".contacts-btn-map");
+
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      mapPopup.classList.add("modal-show");
+    });
+
+    mapClose.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    });
+
+    window.addEventListener("keydown", function (evt) {
+      evt.preventDefault();
+      if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-show")) {
+          mapPopup.classList.remove("modal-show");
+        }
+      }
+    });
